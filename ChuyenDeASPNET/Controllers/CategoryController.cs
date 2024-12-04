80335a28a7648dcb96d55aac6ac1face2ebcf3f8
nguyenvotandung <nguyenvotandungcoi@gmail.com>
2024-12-04 19:33:40 +0700

    first update

---
+++ ChuyenDeASPNET/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChuyenDeASPNET.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult AllCategory()
+        {
+            return View();
+        }
+        public ActionResult ProductByCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChuyenDeASPNET/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChuyenDeASPNET.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult ProfileMain()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+        public ActionResult Seller()
+        {
+            return View();
+        }
+        public ActionResult Setting()
+        {
+            return View();
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+        public ActionResult Address()
+        {
+            return View();
+        }
+    }
+}
